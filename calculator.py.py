b9f4829cc6e7e1bd8df60ff9eb2d1cd147c7eafc
2005tamannasaini-dot <2005tamannasaini@email.com>
2026-07-03 16:00:15 +0530

    Added input validation using try except

--- calculator.py.py
+++ calculator.py.py
@@ -5,42 +5,45 @@ print(" ======== CALCULATOR.🥰 ========")
 while True:
     operation = input("Enter the operation(+,-, *,/,%,power, square,):")
 
-    if operation == "square":
-        num1 = float(input("Enter first number:"))
-        print ("Result:", num1 ** 2)
-
-    else:
-        num1 = float(input("Enter first number:"))
-        num2 = float(input("Enter second number:"))
-
-        
-        if operation== "+":
-            print("Result:",num1+num2)
-
-        elif operation == "-":
-            print ("Result:", num1-num2)
-
-        elif operation == "*":
-            print ("Result:",num1*num2)
-
-        elif operation == "/":
-            if num2 != 0:
-                print("Result:", num1/num2)
-            else:
-                print("cannot divide by zero.")
-
-        elif operation == "%":
-            if num2 != 0:
-                print("Result:", num1%num2)
-            else:
-                print("cannot divide by zero.")
-            
-        
-        elif operation == "power":
-            print("Result:", num1 ** num2)
+    try:
+        if operation == "square":
+            num1 = float(input("Enter first number:"))
+            print ("Result:", num1 ** 2)
 
         else:
-            print("Invalid operation.")
+            num1 = float(input("Enter first number:"))
+            num2 = float(input("Enter second number:"))
+
+            
+            if operation== "+":
+                print("Result:",num1+num2)
+
+            elif operation == "-":
+                print ("Result:", num1-num2)
+
+            elif operation == "*":
+                print ("Result:",num1*num2)
+
+            elif operation == "/":
+                if num2 != 0:
+                    print("Result:", num1/num2)
+                else:
+                    print("cannot divide by zero.")
+
+            elif operation == "%":
+                if num2 != 0:
+                    print("Result:", num1%num2)
+                else:
+                    print("cannot divide by zero.")
+                
+            elif operation == "power":
+                print("Result:", num1 ** num2)
+
+            else:
+                print("Invalid operation.")
+
+    except ValueError:
+        print("Invalid input! Please enter number only.")        
 
     choice = input("continue?(yes/no):").lower()
 
